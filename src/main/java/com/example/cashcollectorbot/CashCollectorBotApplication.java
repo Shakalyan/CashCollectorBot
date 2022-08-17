ee4f23d5ebec657fd1521d9b3aa22e4014d8cd57
Vdovin Victor <vdovitg@gmail.com>
2022-08-17 10:28:52 +0300

    Initial commit

---
+++ src/main/java/com/example/cashcollectorbot/CashCollectorBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.cashcollectorbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashCollectorBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashCollectorBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cashcollectorbot/CashCollectorBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cashcollectorbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashCollectorBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
